88946efcb0709db72858d3f591e91ce66db6befb
Pietro Cagnasso <pietro.cagnasso99@gmail.com>
2022-05-01 10:51:44 +0200

    Add files via upload

--- utils/utils.py
+++ utils/utils.py
@@ -1,75 +1,41 @@
 import copy
 import torch
 from torchvision import datasets, transforms
-from sampling import cifar_iid, cifar_noniid, cifar_iid_unequal
+from sampling import cifar_iid, cifar_noniid, cifar_noniid_unbalanced
 
 
-def get_dataset(iid=1, unbalanced=0, num_users=100):
-    """
-    Returns train and test datasets and a user group which is a dict where
-    the keys are the user index and the values are the corresponding data for
-    each of those users.
-    """
+def get_datasets():
     transform_train = transforms.Compose([
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-        # mean and std of the CIFAR-10 dataset
     ])
-
+    train_dataset = datasets.CIFAR10("./data", train=True, download=True, transform=transform_train)
     transform_test = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-        # mean and std of the CIFAR-10 dataset
     ])
+    test_dataset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
 
-    # choose the training and test datasets
-    train_dataset = datasets.CIFAR10('data', train=True,
-                                     download=True, transform=transform_train)
-    test_dataset = datasets.CIFAR10('data', train=False,
-                                    download=True, transform=transform_test)
+    return train_dataset, test_dataset
 
+def get_user_groups(train_dataset, iid=True, unbalanced=False, tot_users=100):
+    user_groups = None
     if iid:
-        # Sample IID user data from Mnist
-        user_groups = cifar_iid(train_dataset, num_users)
+        user_groups = cifar_iid(train_dataset, tot_users)
     else:
-        # Sample Non-IID user data from Mnist
         if unbalanced:
-            # Chose unequal splits for every user
-            user_groups = cifar_noniid_unbalanced(train_dataset, num_users)
+            user_groups = cifar_noniid_unbalanced(train_dataset, tot_users)
         else:
-            # Chose equal splits for every user
-            user_groups = cifar_noniid(train_dataset, num_users)
+            user_groups = cifar_noniid(train_dataset, tot_users)
 
-    return train_dataset, test_dataset, user_groups
-
+    return user_groups
 
 def average_weights(w):
-    """
-    Returns the average of the weights.
-    """
     w_avg = copy.deepcopy(w[0])
     for key in w_avg.keys():
         for i in range(1, len(w)):
             w_avg[key] += w[i][key]
         w_avg[key] = torch.div(w_avg[key], len(w))
     return w_avg
-
-
-def exp_details(args):
-    print('\nExperimental details:')
-    print(f'    Model     : {args.model}')
-    print(f'    Optimizer : {args.optimizer}')
-    print(f'    Learning  : {args.lr}')
-    print(f'    Global Rounds   : {args.epochs}\n')
-
-    print('    Federated parameters:')
-    if args.iid:
-        print('    IID')
-    else:
-        print('    Non-IID')
-    print(f'    Fraction of users  : {args.frac}')
-    print(f'    Local Batch size   : {args.local_bs}')
-    print(f'    Local Epochs       : {args.local_ep}\n')
-    return
