0e27abc61800d011e9a210101a502fc91bdf7d4a
Pietro Cagnasso <pietro.cagnasso99@gmail.com>
2022-05-28 09:21:41 +0200

    Add files via upload

--- feddyn/components.py
+++ feddyn/components.py
@@ -1,23 +1,17 @@
-from numpy import isin
 import torch
-from torch import nn, optim
+from torch import nn
 from torch.utils.data import DataLoader
-import copy
-from collections import defaultdict
 
 from utils.reproducibility import seed_worker, make_it_reproducible
 from utils.datasets import DatasetSplit
 
 g = torch.Generator()
 
-
-def flatten(model):
-    return troch.cat([param.reshape(-1) for param in model.parameters()])
+client_dir = Path("/content/client_states")
 
 
 class FedDynServer():
     def __init__(self, model, alpha, num_clients, device, testset, seed):
-        make_it_reproducible(seed)
         g.manual_seed(seed)
         
         self.num_clients = num_clients
@@ -29,6 +23,10 @@ class FedDynServer():
         self.h = dict()
         for key in self.model.state_dict():
             self.h[key] = torch.zeros_like(self.model.state_dict()[key])
+
+        for i in range(num_clients):
+            torch.save({"prev_grads": self.h},
+                client_dir / f"{i}.pt")
 
         self.criterion = nn.CrossEntropyLoss()
         self.test_loader = DataLoader(testset, batch_size=100, 
@@ -61,7 +59,7 @@ class FedDynServer():
 
     def evaluate(self, round):
         self.model.eval()
-        print("Evaluating model at round", round, "...", end=" ")
+        print("Evaluating server model at round", round, "...", end=" ")
         
         test_loss = 0
         correct = 0
@@ -92,68 +90,58 @@ class FedDynServer():
 
 
 class FedDynClient():
-    def __init__(self, model, device, lr, wd, mm, alpha, id, local_epochs, trainset, data_idxs):
+    def __init__(self, device, lr, wd, mm, alpha, id, local_epochs, trainset, data_idxs):
         self.device = device
-        self.model = model
         self.id = id
         self.alpha = alpha
         self.lr = lr
         self.wd = wd
         self.mm = mm
         self.local_epochs = local_epochs
-        self.trainset = trainset
-        self.data_idxs = data_idxs
 
         self.criterion = nn.CrossEntropyLoss()
-        self.optim = None
-        self.train_loader = DataLoader(DatasetSplit(self.trainset, self.data_idxs), batch_size=128,
+        self.train_loader = DataLoader(DatasetSplit(trainset, data_idxs), batch_size=128,
             num_workers=2, worker_init_fn=seed_worker, generator=g)
-
-        self.prev_grads = None
-        for param in self.model.parameters():
-            if not isinstance(self.prev_grads, torch.Tensor):
-                self.prev_grads = torch.zeros_like(param.view(-1))
-            else:
-                self.prev_grads = torch.cat((self.prev_grads, torch.zeros_like(param.view(-1))), dim=0)
-        self.prev_grads.to(self.device)
 
         self.train_metrics_list = []
 
-    def train(self, server_state_dict, round):
-        self.model.to(self.device)
-        self.model.load_state_dict(server_state_dict)
-        self.optim = optim.SGD(self.model.parameters(), lr=self.lr, weight_decay=self.wd, momentum=self.mm)
-        self.model.train()
+    def train(self, model, server_state_dict, round):
         print("Training client", self.id, "...", end=" ")
+
+        prev_grads = torch.load(client_dir / f"{id}.pt")["prev_grads"].to(self.device)
+        model.load_state_dict(server_state_dict).to(self.device)
+        optim = nn.optim.SGD(model.parameters(), lr=self.lr, weight_decay=self.wd, momentum=self.mm)
+        model.train()
 
         loss_v = 0
         metrics = []
         for epoch in range(self.local_epochs):
             for img, lbl in self.train_loader:
-                self.optim.zero_grad()
+                optim.zero_grad()
                 img, lbl = img.to(self.device), lbl.to(self.device)
-                y = self.model(img)
+                y = model(img)
                 loss = self.criterion(y, lbl)
                 loss_v += loss.item()
 
-                cur_flat = torch.cat([p.reshape(-1) for p in self.model.parameters()])
+                cur_flat = torch.cat([p.reshape(-1) for p in model.parameters()])
                 # Flatten the current server parameters
-                par_flat = torch.cat([p.reshape(-1) for k, p in server_state_dict.items() if k in [k1 for k1, v in self.model.named_parameters()] ])
+                par_flat = torch.cat([p.reshape(-1) for k, p in server_state_dict.items() if k in [k1 for k1, v in model.named_parameters()] ])
                 #assert(cur_flat.requires_grad)
 
-                lin_penalty = torch.sum(self.prev_grads * cur_flat)
+                lin_penalty = torch.sum(prev_grads * cur_flat)
                 quad_penalty = self.alpha / 2 * torch.linalg.norm((cur_flat - par_flat), 2)**2
 
                 loss = loss - lin_penalty + quad_penalty
                 loss.backward()
-                torch.nn.utils.clip_grad_norm_(parameters=self.model.parameters(), max_norm=10)
-                self.optim.step()
+                torch.nn.utils.clip_grad_norm_(parameters=model.parameters(), max_norm=10)
+                optim.step()
             del img, lbl
                 
         cur_flat = torch.cat([p.detach().reshape(-1) for p in self.model.parameters()])
 
-        self.prev_grads -= self.alpha * (cur_flat - par_flat)
+        prev_grads -= self.alpha * (cur_flat - par_flat)
+        torch.save({"prev_grads": prev_grads},
+            client_dir / f"{self.id}.pt")
                     
         print(f"done! average loss={loss_v/len(self.train_loader)}")
-        self.model.to("cpu")
-        return self.model.state_dict(), metrics
+        return model.state_dict(), metrics
