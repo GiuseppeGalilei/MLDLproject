f8246495026f08f24910bf91b5dfdd90476f17e5
Pietro Cagnasso <pietro.cagnasso99@gmail.com>
2022-06-02 09:37:59 +0200

    Add files via upload

--- feddyn/components.py
+++ feddyn/components.py
@@ -1,207 +1,151 @@
 import torch
-from torch import nn
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
 from torch.utils.data import DataLoader
 import os
-from torch.nn.functional import mse_loss
 
-from utils.reproducibility import seed_worker, make_it_reproducible
+from utils.reproducibility import seed_worker
 from utils.datasets import DatasetSplit
+from fedgkt.utils import *
+
 
 g = torch.Generator()
+clients_status_dir = "./feddyn/clients_status/"
 
-client_dir = "./feddyn/client_states/"
 
-
-class FedDynServer():
-    def __init__(self, model, alpha, num_clients, device, testset, seed):
+class DYNServer():
+    
+    def __init__(self, model, alpha, tot_clients, testset, cuda, seed):
         g.manual_seed(seed)
         
-        self.num_clients = num_clients
+        self.model = model
         self.alpha = alpha
-        self.device = device
-        self.model = model.to(self.device)
-        self.seed = seed
-
+        self.tot_clients = tot_clients
+        self.cuda = cuda
+        
+        if self.cuda:
+            self.model.cuda()
+        
+        self.testld = DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+        
+        # compose server state
         self.h = dict()
         for key in self.model.state_dict():
             self.h[key] = torch.zeros_like(self.model.state_dict()[key])
-
-        if not os.path.exists(client_dir):
-            os.mkdir(client_dir)
-
-        for i in range(num_clients):
-            torch.save({"prev_grads": self.h},
-                client_dir + f"{i}.pt")
-
-        self.criterion = nn.CrossEntropyLoss(reduction="sum")
-        self.test_loader = DataLoader(testset, batch_size=100, 
-            shuffle=False, num_workers=2)
-    
-    def get_model_params_norm(self):
-        total_norm = 0
-        for p in self.model.parameters():
-            param_norm = p.data.norm(2)
-            total_norm += param_norm.item() ** 2
-        total_params_norm = total_norm ** 0.5
-        return total_params_norm
-
-    def update_model(self, active_clients_states):
-        print("Updating server model...", end=" ")
-        num_participants = len(active_clients_states)
-
-        self.h = {
-            key: h - self.alpha / self.num_clients * sum(theta[key] - server_param for theta in active_clients_states)
-            for (key, h), server_param in zip(self.h.items(), self.model.state_dict().values())
-        }
-        
-        par = {
-            key: 1 / num_participants * sum(theta[key] for theta in active_clients_states)
-            for key in self.model.state_dict().keys()
-        }
-        
-        par = {
-            key: param - 1 / self.alpha * h_param
-            for (key, param), h_param in zip(par.items(), self.h.values())
-        }
             
-        self.model.load_state_dict(par)
-        print("done!")
-
-        hasnan = False
-        for key in self.model.state_dict():
-            if torch.isnan(self.model.state_dict()[key]).any():
-                print(f"\t CLient model: {key} has some nan.")
-                hasnan = True
-        if not hasnan:
-            print("\t No nan found in server model!")
+        # generate the clients states
+        if not os.path.exists(clients_status_dir):
+            os.mkdir(clients_status_dir)
+        for cid in range(self.tot_clients):
+            torch.save({"prev_state": self.h}, clients_status_dir + f"{cid}.pt")
+            
+    def update(self, active_devices_models, com_round):
+        print(f"Updating server model @ round {com_round}...", end=" ")
         
-        print(f"\t Server model norm: {self.get_model_params_norm()}")
-
-    def evaluate(self, round):
-        self.model.eval()
-        print("Evaluating server model at round", round, "...", end=" ")
+        num_participants = len(active_devices_models)
         
-        test_loss = 0
-        correct = 0
-        total = 0
-        with torch.no_grad():
-            for img, lbl in self.test_loader:
-                img, lbl = img.to(self.device), lbl.to(self.device)
-                y = self.model(img)
-                test_loss += self.criterion(y, lbl).item()
-                _, predicted = torch.max(y.data, 1)
-                total += lbl.size(0)
-                correct += (predicted == lbl).sum().item()
-
-        metrics = {
-            "round": round,
-            "test_accuracy": correct / total,
-            "test_avg_loss_per_image": test_loss / total
+        self.h = {
+            key: self.h[key] - self.alpha / self.tot_clients * sum(client_model[key] - self.model.state_dicct()[key] for client_model in active_devices_models)
+            for key in self.h
         }
+        
+        params = {
+            key: sum(client_model[key] for client_model in active_clients_models) / num_participants - self.h[key] / self.alpha
+            for key in self.h
+        }
+        
+        self.model.load_state_dict(params)
+        
+        print("done!")
+        
+    def evaluate(self, com_round):
+        print(f"Evaluating server model @ round {com_round}...", end=" ")
+        
+        criterion = nn.CrossEntropyLoss(reduction="sum")
+        
+        loss_value, correct, total = 0, 0, 0
+        
+        self.model.eval()
+        for img, lbl in testld:
+            if self.cuda:
+                img, lbl = img.cuda(), lbl.cuda()
+            lblhat = self.model(img)
+            
+            loss = criterion(lblhat, lbl)
+            loss_value += loss.item()
+            
+            _, predicted = torch.max(lblhat.data, 1)
+            total += lbl.size(0)
+            correct += (predicted == lbl).sum().item()
         self.model.train()
         
-        print("done!")
-        return metrics
-
-    def get_server_state(self):
+        print(f"done!\t accuracy={(correct / total):.3} loss={(loss_value / total):.3}")
+        
+    def get_state_dict(self):
         return self.model.state_dict()
-
-
-class FedDynClient():
-    def __init__(self, device, lr, wd, mm, alpha, id, local_epochs, trainset, data_idxs, clip_value):
-        self.device = device
-        self.id = id
+    
+    
+class DYNClient():
+    
+    def __init__(self, cid, alpha, lr, weight_decay, momentum, clip_value, local_epochs, data_idxs, cuda):
+        self.cid = cid
         self.alpha = alpha
         self.lr = lr
-        self.wd = wd
-        self.mm = mm
-        self.local_epochs = local_epochs
+        self.weight_decay = weight_decay
+        self.momentum = momentum
         self.clip_value = clip_value
-
-        self.criterion = nn.CrossEntropyLoss(reduction="sum")
-        self.train_loader = DataLoader(DatasetSplit(trainset, data_idxs), batch_size=128,
-            num_workers=2, worker_init_fn=seed_worker, generator=g)
-
-    def train(self, model, server_state_dict, round):
-        print("Training client", self.id, "...")
-
-        prev_grads = torch.load(client_dir + f"{self.id}.pt")["prev_grads"]
+        self.local_epochs = local_epochs
+        self.data_idxs = data_idxs
+        self.cuda = cuda
+        
+    def train(self, model, trainset, server_state_dict, com_round):
+        print(f"Training client {self.cid} @ round {com_round}...", end=" ")
+        
         model.load_state_dict(server_state_dict)
-        model.to(self.device)
-        optim = torch.optim.SGD(model.parameters(), lr=self.lr, weight_decay=self.wd, momentum=self.mm)
+        prev_status = torch.load(clients_status_dir + f"{self.cid}.pt")
+        trainld = DataLoader(DatasetSplit(trainset, self.data_idxs), batch_size=128, shuffle=True,
+                             num_workers=2, worker_init_fn=seed_worker, generator=g)
+        if self.cuda:
+            model.cuda()
+            
+        optimizer = optim.SGD(model.parameters(), lr=self.lr, weight_decay=self.weight_decay, momentum=self.momentum)
+        criterion = nn.CrossEntropyLoss(reduction="sum")
+        
+        loss_value, correct, total = 0, 0, 0
+        
         model.train()
-
-        loss_value = 0
-        correct = 0
-        total = 0
         for epoch in range(self.local_epochs):
-            for img, lbl in self.train_loader:
-                optim.zero_grad()
-                img, lbl = img.to(self.device), lbl.to(self.device)
-                y = model(img)
-                loss = self.criterion(y, lbl)
+            for img, lbl in trainld:
+                if self.cuda:
+                    img, lbl = img.cuda(), lbl.cuda()
+
+                optimizer.zero_grad()
+                lblhat = model(img)
+                loss = criterion(lblhat, lbl)
+
+                lin_penalty, quad_penalty = 0, 0
+                for key in model.state_dict():
+                    lin_penalty += prev_status[key] * model.state_dict()[key]
+                    quad_penalty += F.mse_loss(model.state_dict()[key] - server_state_dict[key])
+
+                loss = loss - lin_penalty + quad_penalty
+                loss_value += loss.item()
+
+                loss.backward()
+                optimizer.step()
                 
-                _, predicted = torch.max(y.data, 1)
+                _, predicted = torch.max(lblhat.data, 1)
                 total += lbl.size(0)
                 correct += (predicted == lbl).sum().item()
-
-                lin_penalty = 0
-                quad_penalty = 0
-                for key in model.state_dict().keys():
-                     lin_penalty += torch.sum(prev_grads[key] * model.state_dict()[key])
-                     quad_penalty += mse_loss(model.state_dict()[key].type(torch.DoubleTensor), server_state_dict[key].type(torch.DoubleTensor), reduction='sum')
+        model.eval()
                 
-                print(f"\t loss:{loss.item()}, lin:{lin_penalty}, quad:{quad_penalty}", end="")
-                    
-                loss -= lin_penalty
-                loss += self.alpha / 2. * quad_penalty
-                loss.backward()
-                print(f", modified loss:{loss.item()}")
-                
-                loss_value += loss.item()
-                
-                torch.nn.utils.clip_grad_norm_(parameters=model.parameters(), max_norm=self.clip_value)
-                optim.step()
-            del img, lbl
-
-        with torch.no_grad():
-            for key in model.state_dict():
-                if prev_grads[key].dtype != torch.float32:
-                    prev_grads[key] -= (self.alpha * (model.state_dict()[key] - server_state_dict[key])).long()
-                else:
-                    prev_grads[key] -= self.alpha * (model.state_dict()[key] - server_state_dict[key])
-            torch.save({"prev_grads": prev_grads},
-                client_dir + f"{self.id}.pt")
-            
-        total_norm = 0
-        for p in model.parameters():
-            param_norm = p.data.norm(2)
-            total_norm += param_norm.item() ** 2
-        total_params_norm = total_norm ** 0.5
-        
-        print("\t Client model norm: ", total_params_norm)
-
-        hasnan = False
-        for key in prev_grads:
-            if torch.isnan(prev_grads[key]).any():
-                print(f"\t Client state: {key} has some nan.")
-                hasnan = True
-        if not hasnan:
-            print("\t No nan found in client state!")
-
-        hasnan = False
-        for key in model.state_dict():
-            if torch.isnan(model.state_dict()[key]).any():
-                print(f"\t Client model: {key} has some nan.")
-                hasnan = True
-        if not hasnan:
-            print("\t No nan found in client model!")
-    
-        metrics = {
-            "round": round,
-            "train_avg_loss_per_image": loss_value / total,
-            "train_accuracy": correct / total
+        prev_status = {
+            key: prev_status[key] - self.alpha * (model.state_dict()[key] - server_state_dict[key])
+            for key in prev_status
         }
-                    
-        print(f"done! train loss per image={loss_value / total}\t train accuracy={correct / total}")
-        return model.state_dict(), metrics
+        torch.save(prev_status, clients_status_dir + f"{self.cid}.pt")
+        
+        print(f"done!\t avg_accuracy={(correct / total):.3} avg_loss={(loss_value / total):.3}")
+        
+        return model.state_dict()
