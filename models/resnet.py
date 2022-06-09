686b2cf47b65241f08338d4ee720b678ed22f58e
Pietro Cagnasso <pietro.cagnasso99@gmail.com>
2022-06-09 10:53:35 +0200

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -14,6 +14,33 @@ def Norm(planes, type, num_groups=2):
         return nn.BatchNorm2d(planes)
     elif type == 'Group Norm':
         return nn.GroupNorm(num_groups, planes)
+    
+    
+class Basicblock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1, norm_type="Batch Norm"):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(
+            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = Norm(planes, type=norm_type)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn2 = Norm(planes, type=norm_type)
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion * planes,
+                          kernel_size=1, stride=stride, bias=False),
+                Norm(planes, type=norm_type)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
 
 
 class Bottleneck(nn.Module):
@@ -80,6 +107,10 @@ class ResNet(nn.Module):
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
+
+
+def ResNet18(norm_type="Batch Norm"):
+    return ResNet(Basicblock, [2, 2, 2, 2], norm_type)
 
 
 def ResNet50(norm_type="Batch Norm"):
