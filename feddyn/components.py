84f9356e23d76810f59f42ec57569acc014d966f
Pietro Cagnasso <pietro.cagnasso99@gmail.com>
2022-06-09 11:12:11 +0200

    Update components.py

--- feddyn/components.py
+++ feddyn/components.py
@@ -109,7 +109,7 @@ class FedDynClient():
         self.clip_value = clip_value
 
         self.criterion = nn.CrossEntropyLoss(reduction="sum")
-        self.train_loader = DataLoader(DatasetSplit(trainset, data_idxs), batch_size=50,
+        self.train_loader = DataLoader(DatasetSplit(trainset, data_idxs), batch_size=128,
             num_workers=2, worker_init_fn=seed_worker, generator=g)
         
     def set_lr(self, lr):
