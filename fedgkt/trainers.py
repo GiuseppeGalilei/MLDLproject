b26390dc5524e7dfd5feb410a3f4aba25488abf0
Pietro Cagnasso <pietro.cagnasso99@gmail.com>
2022-07-05 12:07:39 +0200

    Update trainers.py

--- fedgkt/trainers.py
+++ fedgkt/trainers.py
@@ -25,7 +25,7 @@ class GKTServerTrainer(object):
         self.model_global.train()
 
         self.optimizer = optim.Adam(self.model_global.parameters(), lr=1e-3, weight_decay=1e-4)
-        self.scheduler = MultiStepLR(self.optimizer, [15, 30, 45], gamma=0.33)
+        self.scheduler = MultiStepLR(self.optimizer, [20, 30, 40], gamma=0.33)
 
         self.criterion_CE = nn.CrossEntropyLoss()
         self.criterion_KL = KL_Loss(self.args['temperature'])
